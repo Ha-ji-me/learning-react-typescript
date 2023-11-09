879d8f02fad1e41f3fb5fb2b82484a762b98e727
Ha-ji-me <hjmx3599@gmail.com>
2023-11-10 00:54:35 +0900

    ベースコンポーネント

--- src/laravel/LaraTest.tsx
+++ src/laravel/LaraTest.tsx
@@ -1,30 +1,17 @@
 import axios from "axios";
 import { FC, useState, useEffect } from "react";
+import Header from './Header';
+import Footer from './Footer'
+import { Post } from "./Post";
+import PostCard from "./PostCard";
 
 export const LaraTest:FC = () => {
-  // const url: string = 'http://localhost:8000/posts';
-  // const [posts, setPosts] = useState<any>([]);
-  // axios.get(url).then((res) => {
-  //   setPosts(res);
-  //   console.log(posts);
-  // })
-
-  const url = "http://localhost:8000/posts";
-
-  useEffect(()=>{
-    (async ()=>{
-      try{
-        const res = await axios.get(url);
-          console.log(res);
-        return;
-      }catch (e){
-        return e;
-      }
-    })();
-  },[]);
-
   return (
     <div>
+      <Header />
+      <Post />
+      {/* <PostCard /> */}
+      <Footer />
     </div>
   )
 }
